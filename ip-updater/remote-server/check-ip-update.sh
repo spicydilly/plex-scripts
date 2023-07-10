faea72711d6941a0cd1cfce3d01da28207b1530c
spicydilly <dyloncondon@gmail.com>
2023-07-10 02:17:16 +0100

    update ports

--- ip-updater/remote-server/check-ip-update.sh
+++ ip-updater/remote-server/check-ip-update.sh
@@ -1,6 +1,7 @@
 inotifywait -q -m -e close_write /root/home-ip/home-ip.conf |
-while read -r filename event; do
-  export PLEX_URL=$(cat /root/home-ip/home-ip.conf)
-  envsubst '${PLEX_URL}' < /root/home-ip/plex.thedylon.com.template > /etc/nginx/sites-available/plex.thedylon.com
-  sudo systemctl restart nginx.service
-done
+  while read -r filename event; do
+    export PLEX_URL=$(cat /root/home-ip/home-ip.conf)
+    envsubst '${PLEX_URL}' </root/home-ip/plex.thedylon.com.template >/etc/nginx/sites-available/plex.thedylon.com
+    envsubst '${PLEX_URL}' </root/home-ip/tautulli.thedylon.com.template >/etc/nginx/sites-available/tautulli.thedylon.com
+    sudo systemctl restart nginx.service
+  done
